8e973b1616d80f3f7902c340995e0c90953b5342
stanka-n <47890126+stanka-n@users.noreply.github.com>
2019-07-12 15:09:21 +0200

    Update MessageImage.js

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -2,7 +2,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Image, StyleSheet, View, ViewPropTypes } from 'react-native';
+import { Image, StyleSheet, View, ViewPropTypes, ActivityIndicator } from 'react-native';
 import Lightbox from 'react-native-lightbox';
 
 export default function MessageImage({
@@ -14,18 +14,20 @@ export default function MessageImage({
 }) {
   return (
     <View style={[styles.container, containerStyle]}>
-      <Lightbox
-        activeProps={{
-          style: styles.imageActive,
-        }}
-        {...lightboxProps}
-      >
-        <Image
-          {...imageProps}
-          style={[styles.image, imageStyle]}
-          source={{ uri: currentMessage.image }}
-        />
-      </Lightbox>
+      {currentMessage.status === 'uploaded' ? (
+        <Lightbox
+          activeProps={{
+            style: styles.imageActive,
+          }}
+          {...lightboxProps}
+        >
+          <Image
+            {...imageProps}
+            style={[styles.image, imageStyle]}
+            source={{ uri: currentMessage.image }}
+          />
+        </Lightbox>
+      ) : <ActivityIndicator size="small" color="#0000ff" /> }
     </View>
   );
 }
